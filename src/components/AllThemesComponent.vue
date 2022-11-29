e35ce784912aa4ded62c08132fe3f5a4470905b5
Amélie Benoist <amelie.benoist@icloud.com>
2022-11-29 16:23:02 +0100

    themes

---
+++ src/components/AllThemesComponent.vue
@@ -0,0 +1,58 @@
+
+<template>
+    <div class="all">
+        <p>Nos Différents thèmes </p>
+        <div class="themes">
+            <ThemeTousComponent/>
+            <ThemeAnniversaireComponent/>
+            <ThemeBabySComponent/>
+            <ThemeFaTComponent/>
+            <ThemeGenderRevealComponent/>
+            <ThemeMariageComponent/>
+            <ThemeAutreComponent/>
+
+        </div>
+    </div>
+</template>
+
+<script>
+
+import ThemeAnniversaireComponent from './ThemeAnniversaireComponents.vue';
+import ThemeAutreComponent from './ThemeAutreComponent.vue';
+import ThemeBabySComponent from './ThemeBabySComponent.vue'; 
+import ThemeFaTComponent from './ThemeFaTComponent.vue'; 
+import ThemeGenderRevealComponent from './ThemeGenderRevealComponent.vue'; 
+import ThemeMariageComponent from './ThemeMariageComponent.vue'; 
+import ThemeTousComponent from './ThemeTousComponent.vue';
+
+export default {
+    name: 'AllThemesComponent',
+    components: {
+        ThemeAnniversaireComponent,
+        ThemeAutreComponent,
+        ThemeBabySComponent, 
+        ThemeFaTComponent, 
+        ThemeGenderRevealComponent, 
+        ThemeMariageComponent, 
+        ThemeTousComponent, 
+    }
+
+}
+
+</script>
+<style lang="scss" scoped>
+    p{
+        color:#5b4e46;
+        font-size:20px;
+        font-weight: 500;
+        margin-top:100px;
+        text-align: center;
+        text-decoration: underline;
+    }
+
+    div.themes{
+        display: flex;
+        justify-content: space-around;
+    }
+</style>
+
---
+++ src/components/ThemeBabySComponent.vue
@@ -0,0 +1,50 @@
+<template>    
+    <div class="themes">
+        <button type="button" id="bs" @click.prevent="AfficherMasquer">Baby Shower</button>
+        <div id="divacacher" style="display:none;">Baby Shower</div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'ThemeBabySComponent',
+    methods: {
+        AfficherMasquer(){
+            const divInfo = document.getElementById('divacacher');
+            if (divInfo.style.display == 'none')
+            divInfo.style.display = 'block';
+            else
+            divInfo.style.display = 'none';
+        },
+    }
+}
+
+</script>
+
+<style lang="scss" scoped>
+   div.themes{
+        display: flex;
+        margin-top: 50px;
+        justify-content: space-around;
+
+        button{
+            color:#5b4e46;
+            font-size:20px;
+            font-weight: 500;
+            font-family: "Raleway",arial,sans-serif;
+            border: 0.5px solid;
+            border-radius: 10px;
+            border-color: #c9bf93;
+            background-color: white;
+            color:#5b4e46 ;
+            padding: 10px;
+            margin: 5px;
+        }
+
+        button:hover{
+            cursor: pointer;
+        }
+    }
+
+</style>
